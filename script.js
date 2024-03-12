35e45fce226a47aa79f888ae2674e302e0e4e9de
Debarjit mohanty <91021174+Debarjitmohanty@users.noreply.github.com>
2024-03-12 16:21:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// hide top-bar
+$(document).ready(function() {
+    // Smooth scrolling for anchor links
+    $('a[href^="#"]').on('click', function(event) {
+        var target = $(this.getAttribute('href'));
+        if (target.length) {
+            event.preventDefault();
+            $('html, body').stop().animate({
+                scrollTop: target.offset().top
+            }, 1000); // Adjust the duration as needed
+        }
+    });
+});
